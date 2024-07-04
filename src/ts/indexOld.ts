e25254301e0859d8e41142277ce5642bac475cb5
RyanGripp <RyanGripp@users.noreply.github.com>
2024-07-04 15:34:38 -0300

    add firebase

--- src/ts/indexOld.ts
+++ src/ts/indexOld.ts
@@ -1,6 +1,3 @@
-import firebase from "firebase/compat/app";
-import "firebase/compat/database";
-
 import { Product } from "./Product";
 import { createFilters } from "./view-filter";
 import { createCart } from "./view-cart";
@@ -8,18 +5,7 @@ import { displayProducts } from "./view-products";
 import { createOrder } from "./view-order";
 import { OptionsMobile } from "./view-mobile";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBlfG6w8r-shyQV7htBLwtb_LPVCAE9vqQ",
-  authDomain: "produtos-cadastra.firebaseapp.com",
-  databaseURL: "https://produtos-cadastra-default-rtdb.firebaseio.com",
-  projectId: "produtos-cadastra",
-  storageBucket: "produtos-cadastra.appspot.com",
-  messagingSenderId: "220465147283",
-  appId: "1:220465147283:web:9633e244c79c2d71dc0c66"
-};
-
-firebase.initializeApp(firebaseConfig);
-const database = firebase.database();
+const serverUrl = "http://localhost:5000";
 
 document.addEventListener("DOMContentLoaded", async () => {
   const products: Product[] = await loadProducts(); //Carregar os produtos
@@ -28,13 +14,13 @@ document.addEventListener("DOMContentLoaded", async () => {
   createOrder(products); //Criar a ordenação dos produtos
   createCart(); //Criar o carrinho
   OptionsMobile(products); //Criar as opções para mobile
+  console.log(serverUrl);
 });
 
 async function loadProducts(): Promise<Product[]> {
-  const snapshot = await database.ref('/products').once('value');
-  const products = snapshot.val();
-  if (!products) {
-    throw new Error("Nenhum produto encontrado");
+  const response = await fetch(serverUrl + "/products");
+  if (!response.ok) {
+    throw new Error("Erro ao carregar");
   }
-  return products;
+  return response.json();
 }
